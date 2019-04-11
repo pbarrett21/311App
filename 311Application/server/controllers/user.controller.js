7ac0f2f2306c94c6643c6e120c475354c941299a
kwilliamsuky <35701248+kwilliamsuky@users.noreply.github.com>
2019-04-11 18:24:21 -0400

    Add location to user profile

--- 311Application/server/controllers/user.controller.js
+++ 311Application/server/controllers/user.controller.js
@@ -45,7 +45,7 @@ module.exports.userProfile = (req, res, next) => {
                 return res.status(404).json({ status: false, message: 'User record not found.'});
             }
             else{
-                return res.status(200).json({status: true, user : _.pick(user, ['fullName', 'email']) });
+                return res.status(200).json({status: true, user : _.pick(user, ['fullName', 'email', 'userLocation']) });
             }
         });
 }
